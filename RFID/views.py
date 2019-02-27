48d1d78bfd1063dc793f61cebccc305f5d69c13b
Ali <amusavi15@earlham.edu>
2019-02-27 00:35:41 -0500

    Finished everything with objects and users

--- RFID/views.py
+++ RFID/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -178,7 +179,10 @@ class GetObjectPageView(LoginRequiredMixin, View):
 
         if form['object_id'].value():
             id = form['object_id'].value()
-            obj = Object.objects.get(object_id=id)
+            try:
+                obj = Object.objects.get(object_id=id)
+            except ObjectDoesNotExist:
+                obj = None
             check = False
             count = 1
             return (obj, count, check)
@@ -194,25 +198,29 @@ class GetObjectPageView(LoginRequiredMixin, View):
     def post(self, request):
         get_object_form = GetObject(request.POST)
         querySet, count, check = self.get_object(get_object_form)
+        if querySet is None:
+            message = "The object does not exist!"
+        else:
+            message = ""
 
         # If this is just one object
         if check is False:
             form = AddObject(instance=querySet)
-            context = {'form': form}
+            context = {'form': form, 'message': message}
             return render(request, self.single_object_template, context)
 
         else:
             form = GetGroupObject(instance=querySet)
-            context = {'form':form, 'count':count}
+            context = {'form':form, 'count':count, 'message':message}
             return render(request, self.group_object_template, context)
 
 
 
 
 class UpdateSingleObjectView(LoginRequiredMixin, View):
-    """ Gets the object ID or name and redirects to
-    update page, where user can delete or update on object
-    or a set of objects
+    """ Gets the object ID, updates the specific objects
+    information or deletes it. Redirects the user to the
+    main objects page.
     """
 
     login_url = '/RFID/login/'
@@ -249,14 +257,32 @@ class UpdateSingleObjectView(LoginRequiredMixin, View):
 
 
 class UpdateGroupObjectView(LoginRequiredMixin, View):
-    """ Gets the object ID or name and redirects to
-    update page, where user can delete or update on object
-    or a set of objects
+    """ Gets the object name, updates the group of objects
+    information or deletes them. Redirects the user to the
+    main objects page.
     """
 
     login_url = '/RFID/login/'
     redirect_field_name = ''
-    template = 'RFID/object.html'
+    template_name = 'RFID/object.html'
 
     def post(self, request):
-        return render(request, self.template)
+        form = GetGroupObject(request.POST)
+        name = form['name'].value()
+
+        if 'Update' in request.POST:
+            Object.objects.filter(name=name).update(
+            name = form['name'].value(),
+            availability = form['availability'].value(),
+            max_time = form['max_time'].value(),
+            )
+            message = 'Information for the group of objects was successfuly updated'
+
+        elif 'Delete' in request.POST:
+            Object.objects.filter(name=name).delete()
+            message = 'Group of objects were successfully deleted'
+
+        get_id_form = GetObject()
+        form = AddObject()
+        context = {'add_object_form':form, 'get_object_form':get_id_form, 'message':message}
+        return render(request, self.template_name, context)
